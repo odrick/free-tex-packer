f47a5a4fa1cf289fae81ad3d47fca0c87bad3760
alexander.norinchak <norinchak@gmail.com>
2017-04-12 21:20:12 +0300

    donate button

--- src/client/ui/MainHeader.jsx
+++ src/client/ui/MainHeader.jsx
@@ -15,12 +15,22 @@ class MainHeader extends React.Component {
     render() {
         return (
             <div className="main-header back-900 color-white">
-                <div className="main-header-left">
+                <div className="main-header-app-name">
                     <img src="static/images/logo.png" />
                     {appInfo.displayName} {appInfo.version}
                 </div>
-                <div className="main-header-right">
-                    <div className="main-header-about" onClick={this.showAbout} >?</div>
+
+                <div className="main-header-about" onClick={this.showAbout}>
+                    ?
+                </div>
+
+                <div className="main-header-donate">
+                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                        <input type="hidden" name="cmd" value="_s-xclick"/>
+                        <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHNwYJKoZIhvcNAQcEoIIHKDCCByQCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYAAdYdKKNxkshfJ6pILwLxs9aBT7xY8jfeeG2xLgIs1esEaiaSeRwra4jc/Q9A8pVvUoP8XDBwuzvWVwaUKgAFq7JhB9vwgpbuhsJKbcJw0+5eYl5zdePwbwBNKGyhJYU8WKjyUVSIwSQi2KOOQBAolVVEraVH5aWb1+Xn0hb+3FTELMAkGBSsOAwIaBQAwgbQGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIZObeGU8p2jeAgZCxJodksfDwJrv1aJj7a4FxZsok49ZMHqva8vrnWc7HcH2rK/Tq1Fx6Lu+33JQBhF/qDmx5gX21gHIsqx4fIOkaXjhcVuMLwdb3maqkycYhVxtHs+0G99lF+N2e5v8jhBcNrpNuclyY+LzjXYZ00KaYih3i9kVG7p8Xv15gPzAcLYutDkbfe8G+WZm41sniFxigggOHMIIDgzCCAuygAwIBAgIBADANBgkqhkiG9w0BAQUFADCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wHhcNMDQwMjEzMTAxMzE1WhcNMzUwMjEzMTAxMzE1WjCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAMFHTt38RMxLXJyO2SmS+Ndl72T7oKJ4u4uw+6awntALWh03PewmIJuzbALScsTS4sZoS1fKciBGoh11gIfHzylvkdNe/hJl66/RGqrj5rFb08sAABNTzDTiqqNpJeBsYs/c2aiGozptX2RlnBktH+SUNpAajW724Nv2Wvhif6sFAgMBAAGjge4wgeswHQYDVR0OBBYEFJaffLvGbxe9WT9S1wob7BDWZJRrMIG7BgNVHSMEgbMwgbCAFJaffLvGbxe9WT9S1wob7BDWZJRroYGUpIGRMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbYIBADAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBQUAA4GBAIFfOlaagFrl71+jq6OKidbWFSE+Q4FqROvdgIONth+8kSK//Y/4ihuE4Ymvzn5ceE3S/iBSQQMjyvb+s2TWbQYDwcp129OPIbD9epdr4tJOUNiSojw7BHwYRiPh58S1xGlFgHFXwrEBb3dgNbMUa+u4qectsMAXpVHnD9wIyfmHMYIBmjCCAZYCAQEwgZQwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tAgEAMAkGBSsOAwIaBQCgXTAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0xNzA0MTIxNzU1MTNaMCMGCSqGSIb3DQEJBDEWBBTt4IpOU/lSaCsYLBSExJCyKLUaoTANBgkqhkiG9w0BAQEFAASBgGmP/fAfz5K7viCXIMUj5WGHnUzMkZZjKxj9Qxmj7QwWxOZezoMUbXld9JIBIVhOnBcMHMEI44wL9TERqvbt3PFQQst6mC4f3preZCbMmqK67JM65bYr75j35ERXt6/OzOjrnkZEuH1PLYvcJGuR3BhFASBijGGsb5FOLDSVxaMh-----END PKCS7-----"/>
+                        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"/>
+                        <img alt="" border="0" src="https://www.paypalobjects.com/ru_RU/i/scr/pixel.gif" width="1" height="1"/>
+                    </form>
                 </div>
             </div>
         );
