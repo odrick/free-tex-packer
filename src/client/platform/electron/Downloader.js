336a8885b9103a3bafff30fe73ceb303a59e5a9c
Alexander Norinchak <norinchak@gmail.com>
2019-12-12 16:32:01 +0200

    Bugfix #27

--- src/client/platform/electron/Downloader.js
+++ src/client/platform/electron/Downloader.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const {dialog} = require('electron').remote;
 
 class Downloader {
@@ -14,11 +15,25 @@ class Downloader {
         }
         
         if(dir) {
+            dir = String(dir);
             for(let file of files) {
                 let content = file.content;
                 if(file.base64) content = Buffer.from(content, 'base64');
+
+                let savePath = path.normalize(dir + "/" + file.name);
+                savePath = savePath.split("\\").join("/");
                 
-                fs.writeFileSync(dir + "/" + file.name, content);
+                let saveDirParts = savePath.split("/");
+                saveDirParts.pop();
+                let currentPath = '';
+                while(saveDirParts.length) {
+                    currentPath = currentPath + saveDirParts.shift() + '/';
+                    if(!fs.existsSync(currentPath)) {
+                        fs.mkdirSync(currentPath);
+                    }
+                }
+                
+                fs.writeFileSync(savePath, content);
             }
         }
     }
