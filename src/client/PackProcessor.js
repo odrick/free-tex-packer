2fcae6748e37f0a169a320b94a2a87caade162bc
Alexander Norinchak <norinchak@gmail.com>
2021-04-28 18:11:33 +0300

    Fix some issues

--- src/client/PackProcessor.js
+++ src/client/PackProcessor.js
@@ -11,39 +11,39 @@ class PackProcessor {
     static detectIdentical(rects) {
 
         let identical = [];
-        
-        for(let i=0; i<rects.length; i++) {
+
+        for (let i = 0; i < rects.length; i++) {
             let rect1 = rects[i];
-            for(let n=i+1; n<rects.length; n++) {
+            for (let n = i + 1; n < rects.length; n++) {
                 let rect2 = rects[n];
-                if(rect1.image._base64 === rect2.image._base64 && identical.indexOf(rect2) < 0) {
+                if (rect1.image._base64 === rect2.image._base64 && identical.indexOf(rect2) < 0) {
                     rect2.identical = rect1;
                     identical.push(rect2);
                 }
             }
         }
-        
-        for(let rect of identical) {
+
+        for (let rect of identical) {
             rects.splice(rects.indexOf(rect), 1);
         }
-        
+
         return {
             rects: rects,
             identical: identical
         }
     }
-    
+
     static applyIdentical(rects, identical) {
         let clones = [];
         let removeIdentical = [];
-        
-        for(let item of identical) {
+
+        for (let item of identical) {
             let ix = rects.indexOf(item.identical);
-            if(ix >= 0) {
+            if (ix >= 0) {
                 let rect = rects[ix];
-                
+
                 let clone = Object.assign({}, rect);
-                
+
                 clone.name = item.name;
                 clone.image = item.image;
                 clone.originalFile = item.file;
@@ -54,19 +54,19 @@ class PackProcessor {
             }
         }
 
-        for(let item of removeIdentical) {
+        for (let item of removeIdentical) {
             identical.splice(identical.indexOf(item), 1);
         }
-        
-        for(let item of clones) {
+
+        for (let item of clones) {
             item.cloned = true;
             rects.push(item);
         }
-        
+
         return rects;
     }
-    
-    static pack(images={}, options={}, onComplete=null, onError=null) {
+
+    static pack(images = {}, options = {}, onComplete = null, onError = null) {
 
         let rects = [];
 
@@ -75,13 +75,13 @@ class PackProcessor {
 
         let maxWidth = 0, maxHeight = 0;
         let minWidth = 0, minHeight = 0;
-        
+
         let alphaThreshold = options.alphaThreshold || 0;
-        if(alphaThreshold > 255) alphaThreshold = 255;
+        if (alphaThreshold > 255) alphaThreshold = 255;
 
         let names = Object.keys(images).sort();
-        
-        for(let key of names) {
+
+        for (let key of names) {
             let img = images[key];
 
             let name = key.split(".")[0];
@@ -89,60 +89,60 @@ class PackProcessor {
             maxWidth += img.width;
             maxHeight += img.height;
 
-            if(img.width > minWidth) minWidth = img.width + padding*2 + extrude*2;
-            if(img.height > minHeight) minHeight = img.height + padding*2 + extrude*2;
+            if (img.width > minWidth) minWidth = img.width + padding * 2 + extrude * 2;
+            if (img.height > minHeight) minHeight = img.height + padding * 2 + extrude * 2;
 
             rects.push({
-                frame: {x: 0, y: 0, w: img.width, h: img.height},
+                frame: { x: 0, y: 0, w: img.width, h: img.height },
                 rotated: false,
                 trimmed: false,
-                spriteSourceSize: {x: 0, y: 0, w: img.width, h: img.height},
-                sourceSize: {w: img.width, h: img.height},
+                spriteSourceSize: { x: 0, y: 0, w: img.width, h: img.height },
+                sourceSize: { w: img.width, h: img.height },
                 name: name,
                 file: key,
                 image: img
             });
         }
 
-        let width = options.width || 0;
-        let height = options.height || 0;
+        let width = options.width / options.scale || 0;
+        let height = options.height / options.scale || 0;
 
-        if(!width) width = maxWidth;
-        if(!height) height = maxHeight;
+        if (!width) width = maxWidth;
+        if (!height) height = maxHeight;
 
         if (options.powerOfTwo) {
-            let sw = Math.round(Math.log(width)/Math.log(2));
-            let sh = Math.round(Math.log(height)/Math.log(2));
-			
-			let pw = Math.pow(2, sw);
+            let sw = Math.round(Math.log(width) / Math.log(2));
+            let sh = Math.round(Math.log(height) / Math.log(2));
+
+            let pw = Math.pow(2, sw);
             let ph = Math.pow(2, sh);
-			
-			if(pw < width) pw = Math.pow(2, sw + 1);
-			if(ph < height) ph = Math.pow(2, sh + 1);
-			
-			width = pw;
-			height = ph;
+
+            if (pw < width) pw = Math.pow(2, sw + 1);
+            if (ph < height) ph = Math.pow(2, sh + 1);
+
+            width = pw;
+            height = ph;
         }
 
-        if(width < minWidth || height < minHeight) {
-            if(onError) onError({
-                description: I18.f("INVALID_SIZE_ERROR", minWidth, minHeight)
+        if (width < minWidth || height < minHeight) {
+            if (onError) onError({
+                description: I18.f("INVALID_SIZE_ERROR", Math.round(minWidth * options.scale), Math.round(minHeight * options.scale))
             });
             return;
         }
 
-        if(options.allowTrim) {
+        if (options.allowTrim) {
             Trimmer.trim(rects, alphaThreshold);
         }
 
-        for(let item of rects) {
-            item.frame.w += padding*2 + extrude*2;
-            item.frame.h += padding*2 + extrude*2;
+        for (let item of rects) {
+            item.frame.w += padding * 2 + extrude * 2;
+            item.frame.h += padding * 2 + extrude * 2;
         }
-        
+
         let identical = [];
-        
-        if(options.detectIdentical) {
+
+        if (options.detectIdentical) {
             let res = PackProcessor.detectIdentical(rects);
 
             rects = res.rects;
@@ -154,9 +154,9 @@ class PackProcessor {
             for (let packerClass of allPackers) {
                 if (packerClass !== OptimalPacker) {
                     for (let method in packerClass.methods) {
-                        methods.push({packerClass, packerMethod: packerClass.methods[method], allowRotation: false});
-                        if(options.allowRotation) {
-                            methods.push({packerClass, packerMethod: packerClass.methods[method], allowRotation: true});
+                        methods.push({ packerClass, packerMethod: packerClass.methods[method], allowRotation: false });
+                        if (options.allowRotation) {
+                            methods.push({ packerClass, packerMethod: packerClass.methods[method], allowRotation: true });
                         }
                     }
                 }
@@ -166,7 +166,7 @@ class PackProcessor {
 
         let packerClass = options.packer || MaxRectsBinPack;
         let packerMethod = options.packerMethod || MaxRectsBinPack.methods.BestShortSideFit;
-        let packerCombos = (packerClass === OptimalPacker) ? getAllPackers() : [{packerClass, packerMethod, allowRotation: options.allowRotation}];
+        let packerCombos = (packerClass === OptimalPacker) ? getAllPackers() : [{ packerClass, packerMethod, allowRotation: options.allowRotation }];
 
         let optimalRes;
         let optimalSheets = Infinity;
@@ -200,24 +200,24 @@ class PackProcessor {
                 }
             }) : identical;
 
-            while(_rects.length) {
+            while (_rects.length) {
                 let packer = new combo.packerClass(width, height, combo.allowRotation);
                 let result = packer.pack(_rects, combo.packerMethod);
 
-                for(let item of result) {
+                for (let item of result) {
                     item.frame.x += padding + extrude;
                     item.frame.y += padding + extrude;
-                    item.frame.w -= padding*2 + extrude*2;
-                    item.frame.h -= padding*2 + extrude*2;
+                    item.frame.w -= padding * 2 + extrude * 2;
+                    item.frame.h -= padding * 2 + extrude * 2;
                 }
 
-                if(options.detectIdentical) {
+                if (options.detectIdentical) {
                     result = PackProcessor.applyIdentical(result, _identical);
                 }
 
                 res.push(result);
 
-                for(let item of result) {
+                for (let item of result) {
                     this.removeRect(_rects, item.name);
                 }
 
@@ -235,14 +235,14 @@ class PackProcessor {
             }
         }
 
-        if(onComplete) {
+        if (onComplete) {
             onComplete(optimalRes);
         }
     }
 
     static removeRect(rects, name) {
-        for(let i=0; i<rects.length; i++) {
-            if(rects[i].name === name) {
+        for (let i = 0; i < rects.length; i++) {
+            if (rects[i].name === name) {
                 rects.splice(i, 1);
                 return;
             }
--- src/client/packers/MaxRectsPacker.js
+++ src/client/packers/MaxRectsPacker.js
@@ -8,12 +8,12 @@ const METHOD = {
     SmartArea: "SmartArea",
     Square: "Square",
     SquareArea: "SquareArea",
-    SmartSquare: "SmartSquare",
-    SmartSquareArea: "SmartSquareArea"
+    // SmartSquare: "SmartSquare",
+    // SmartSquareArea: "SmartSquareArea"
 };
 
 class MaxRectsPacker extends Packer {
-    constructor(width, height, allowRotate=false) {
+    constructor(width, height, allowRotate = false) {
         super();
 
         this.binWidth = width;
@@ -33,19 +33,19 @@ class MaxRectsPacker extends Packer {
         let packer = new MaxRectsPackerEngine(this.binWidth, this.binHeight, 0, options);
 
         let input = [];
-        
-        for(let item of data) {
-            input.push({width: item.frame.w, height: item.frame.h, data: item});
+
+        for (let item of data) {
+            input.push({ width: item.frame.w, height: item.frame.h, data: item });
         }
 
         packer.addArray(input);
-        
+
         let bin = packer.bins[0];
         let rects = bin.rects;
-        
+
         let res = [];
-        
-        for(let item of rects) {
+
+        for (let item of rects) {
             item.data.frame.x = item.x;
             item.data.frame.y = item.y;
             if (item.rot) {
@@ -53,10 +53,10 @@ class MaxRectsPacker extends Packer {
             }
             res.push(item.data);
         }
-        
+
         return res;
     }
-    
+
     static get type() {
         return "MaxRectsPacker";
     }
@@ -65,20 +65,20 @@ class MaxRectsPacker extends Packer {
         return METHOD;
     }
 
-    static getMethodProps(id='') {
-        switch(id) {
+    static getMethodProps(id = '') {
+        switch (id) {
             case METHOD.Smart:
-                return {name: "Smart edge logic", description: ""};
+                return { name: "Smart edge logic", description: "" };
             case METHOD.SmartArea:
-                return {name: "Smart area logic", description: ""};
+                return { name: "Smart area logic", description: "" };
             case METHOD.Square:
-                return {name: "Square edge logic", description: ""};
+                return { name: "Square edge logic", description: "" };
             case METHOD.SquareArea:
-                return {name: "Square area logic", description: ""};
+                return { name: "Square area logic", description: "" };
             case METHOD.SmartSquare:
-                return {name: "Smart square edge logic", description: ""};
+                return { name: "Smart square edge logic", description: "" };
             case METHOD.SmartSquareArea:
-                return {name: "Smart square area logic", description: ""};
+                return { name: "Smart square area logic", description: "" };
             default:
                 throw Error("Unknown method " + id);
         }
